f1522e2b29a5c6f92fcdec2e5f9d63930f8fc120
logan1x <sharmakhushal78@gmail.com>
2023-12-13 17:43:02 +0530

    Fix : added check for app env to check if running in dev or prod

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -2,14 +2,18 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	env := os.Getenv("APP_ENV")
+	if env == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 }
